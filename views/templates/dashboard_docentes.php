9cfa1ed8625fb5e4ec3e77ef89e1b67b8d337511
GervacioVaquero777 <2021451042@teschi.edu.mx>
2024-10-16 12:33:58 -0600

    maquetado de perfil 16Oct24

--- views/templates/dashboard_docentes.php
+++ views/templates/dashboard_docentes.php
@@ -73,7 +73,7 @@ $usuario['antiguedad'] = $antiguedad;
   <link rel="stylesheet" href="css/daterangepicker.css" />
   <!-- App CSS -->
   <link rel="stylesheet" href="css/app-light.css" id="lightTheme">
-  <link rel="stylesheet" href="css/app-dark.css" id="darkTheme" disabled>
+  <link rel="stylesheet" href="css/app-dark.css" id="darkTheme">
   </link>
 
 <!-- Bootstrap JS -->
@@ -117,7 +117,7 @@ $usuario['antiguedad'] = $antiguedad;
             </span>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Profile</a>
+            <a class="dropdown-item" href="Perfil.php">Profile</a>
             <a class="dropdown-item" href="#">Settings</a>
             <a class="dropdown-item" href="#">Activities</a>
             <form method="POST" action="" id="logoutForm">
